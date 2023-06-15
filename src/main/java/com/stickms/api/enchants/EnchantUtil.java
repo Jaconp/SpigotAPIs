14d1c92ac76ef7fca01dbc41a0a51c9f7f857930
Jaconp <122712543+Jaconp@users.noreply.github.com>
2023-06-15 17:10:10 +0800

    Added EnchantUtil#applyEnchantment
    Added the applyEnchantment function that will not only add the enchantment but also add the lore.

--- src/main/java/com/stickms/api/enchants/EnchantUtil.java
+++ src/main/java/com/stickms/api/enchants/EnchantUtil.java
@@ -1,15 +1,21 @@
 package com.stickms.api.enchants;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
+@SuppressWarnings("ConstantConditions")
 public class EnchantUtil {
     public static boolean areEqual(Enchantment first, Enchantment second){
         if (first == null || second == null) return false;
@@ -26,7 +32,6 @@ public class EnchantUtil {
         }
         return true;
     }
-
     public static void deregister(NamespacedKey enchantKey){
         Enchantment enchantment = Enchantment.getByKey(enchantKey);
         if (enchantment == null) return;
@@ -49,5 +54,30 @@ public class EnchantUtil {
             HashMap<String, Enchantment> byName = (HashMap<String, Enchantment>) nameField.get(null);
             byName.remove(enchantment.getName());
         } catch (NoSuchFieldException | IllegalAccessException ignored) { }
+    }
+    public static ItemStack applyEnchantment(ItemStack itemStack, Enchantment enchantment, int level){
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.addEnchant(enchantment, level, true);
+        if (Arrays.asList(Enchantment.values()).contains(enchantment)){
+            itemStack.setItemMeta(itemMeta);
+            return itemStack;
+        }
+        if (!itemMeta.hasLore()) {
+            itemMeta.setLore(Collections.singletonList(ChatColor.GRAY + enchantment.getName() + " " + romanNumeral(level)));
+        } else {
+            List<String> lore = itemMeta.getLore();
+            lore.add(ChatColor.GRAY + enchantment.getName() + " " + romanNumeral(level));
+            itemMeta.setLore(lore);
+        }
+        itemStack.setItemMeta(itemMeta);
+        return itemStack;
+    }
+    public static String romanNumeral(int level){
+        String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] hrns = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] ths = {"", "M", "MM", "MMM"};
+
+        return ths[level / 1000] + hrns[(level % 1000) / 100] + tens[(level % 100) / 10] + ones[level % 10];
     }
 }
