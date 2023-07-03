c02a65c781114a221a2327f7e39ef7659902327f
Jaconp <122712543+Jaconp@users.noreply.github.com>
2023-07-03 17:26:55 +0800

    Fixed bug
    Forgot to add the import for PlayerInteractEvent

--- src/main/java/com/stickms/api/enchants/EnchantListenerImpl.java
+++ src/main/java/com/stickms/api/enchants/EnchantListenerImpl.java
@@ -5,6 +5,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockDropItemEvent;
 import org.bukkit.event.entity.*;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 
 public abstract class EnchantListenerImpl extends EnchantListener{
